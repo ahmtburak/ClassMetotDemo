2a1de4302b6eb321c0f8a2cbe07b713e93d5e296
ahmtburak <matrixkrista1@gmail.com>
2021-03-19 17:57:19 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " "+ musteri.Soyad +" artık yeni müşterimizsiniz.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Id'si " +musteri.Id + " olan kullanıcı silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyad = "Çetin";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Betül";
+            musteri2.Soyad = "Çetin";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri ID: " + musteri.Id);
+                Console.WriteLine("Müşteri Ad: " + musteri.Ad);
+                Console.WriteLine("Müşteri Soyad: " + musteri.Soyad);
+                Console.WriteLine("*************");
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+        }
+    }
+}
